659e519b640c45cf480294722cd0e0e0b20e57de
rasel-machette <79604970+rasel-machette@users.noreply.github.com>
2021-04-22 12:37:41 +0600

    Add files via upload

---
+++ es.limits-service.qubist.service/src/main/java/com/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,20 @@
+package com.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.limitsservice.bean.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limit")
+	public LimitConfiguration retrievelLimitsFromConfiguration() {
+		//return new LimitConfiguration(1000,1);
+		return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
+	} 
+
+}
